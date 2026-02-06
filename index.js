93415d2ea3757560398ec8ff61cc5786b496175c
Luke Linington <lukelinington@gmail.com>
2026-02-06 17:40:10 +0000

    add lovelyParaAttacher

--- index.js
+++ index.js
@@ -6,11 +6,17 @@
 
 
 function targetTextToConsole(event){
-    const button = document.querySelector('#button0')
-    console.log(button.textContent)
+    const button = document.querySelector('#button0');
+    console.log(button.textContent);
 }
 
 function tttcAttacher(){
-    const button = document.querySelector('#button0')
-    button.addEventListener('click', targetTextToConsole)
+    const button = document.querySelector('#button0');
+    button.addEventListener('click', targetTextToConsole);
+}
+
+function lovelyParaAttacher(){
+    const paraElement = document.querySelector('#thisisalovelyparagraph');
+    paraElement.addEventListener('click', lovelyToggle);
+
 }
