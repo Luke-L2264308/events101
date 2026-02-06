44bafe2a3a3c6d93b3cbdb72b7e9cdbc0e290a68
Luke Linington <lukelinington@gmail.com>
2026-02-06 17:32:39 +0000

    add targetTextToConsole and tttcattacher

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 /**
  * Add your functions here...
  */
+
+
+
+function targetTextToConsole(event){
+    const button = document.querySelector('#button0')
+    console.log(button.textContent)
+}
+
+function tttcAttacher(){
+    const button = document.querySelector('#button0')
+    button.addEventListener('click', targetTextToConsole)
+}
